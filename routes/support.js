a9c65a6cc40fb72bea5911aa671837675313ee11
redqoralsrl <redqoralsrl@gmail.com>
2021-06-23 15:22:17 +0900

    support error catch

--- routes/support.js
+++ routes/support.js
@@ -4,9 +4,18 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('support',{
-        title : ejs.render('title')
-    });
+    if(req.session.logined == true){
+        res.render('support',{
+            logined : true,
+            title : ejs.render('title')
+        });
+    }else{
+        res.render('support',{
+            logined : false,
+            title : ejs.render('title')
+        });
+    }
+    
 });
 
 module.exports = router;

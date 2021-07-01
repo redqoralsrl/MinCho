71f863a98c24c3c74a51f5ea6ac1f9817f6ee8c5
redqoralsrl <redqoralsrl@gmail.com>
2021-07-01 09:33:50 +0900

    commit

--- routes/trade.js
+++ routes/trade.js
@@ -20,7 +20,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/number', function(req,res,next){
-    console.log('dataaaaaaaa', req.body.percent);
     client.query('select * from userdb where id=?',[req.session.userId],(err,data)=>{
         if(err) console.log(err);
         const result = data[0].balance * (req.body.percent / 100);
@@ -29,16 +28,19 @@ router.post('/number', function(req,res,next){
 })
 
 router.post('/buy', function(req,res,next){
-    // console.log(req.body);
-    // const coin_price = req.body.coin_value;
-    // const user_money = req.body.number;
-    // client.query('select * from userdb where id=?',[req.session.userId],(err,data)=>{
-    //     if(err) console.log(err);
-    //     const change_money = data[0].balance - user_money;
-    //     client.query('update userdb set balance=? where id=?',[],(err,data)=>{
-
-    //     })
-    // })
+    const coin_price = req.body.coin_value;
+    const user_money = req.body.number;
+    client.query('select * from userdb where id=?',[req.session.userId],(err,data)=>{
+        if(err) console.log(err);
+        if(data[0].balance < user_money) res.send("<script>alert('보유 돈이 부족합니다'); history.back();</script>")
+        else{
+            const change_money = data[0].balance - user_money;
+            client.query('update userdb set balance=? where id=?',[change_money, req.session.userId],(err,data)=>{
+                req.session.balance = change_money;
+                req.session.save();
+            })
+        }
+    })
 });
 
 router.post('/sell', function(req, res, next){

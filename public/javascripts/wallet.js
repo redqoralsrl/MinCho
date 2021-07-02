94f7fc4c3515bab5c4afa89914b2f8f38ed8882a
kyjin8 <kyjin9705@gmail.com>
2021-07-02 15:33:18 +0900

    wallet에도 시세표 붙임, trade 페이지 추가 수정

--- public/javascripts/wallet.js
+++ public/javascripts/wallet.js
@@ -51,14 +51,13 @@ function setUpbitData(){
 
                 // console.log('tlqkf',tickers[i]);
 
-                let rowHtml = `<tr><td id=\"list_${arr_english_name[i]}\">` + arr_english_name[i] + "</td>";
-                 rowHtml += "<td>" + arr_market_name[i] + "</td>";
+                let rowHtml = `<tr><td id=\"list_${arr_english_name[i]}\" class="en_name">` + arr_english_name[i] + "<br/><em>" + arr_market_name[i] + "<em/></td>";
                 rowHtml += `<td id=\"${arr_english_name[i]}\">` + comma(tickers[i].trade_price)+"</td>";
                 rowHtml += "<td>" + comma((tickers[i].signed_change_rate*100).toFixed(2))+"</td>";
                 rowHtml += "<td>" 
                 + comma((tickers[i].acc_trade_price_24h > 1000000 ? ( tickers[i].acc_trade_price_24h / 1000000 ) : tickers[i].acc_trade_price_24h).toFixed(0)) 
-                + (tickers[i].acc_trade_price_24h > 1000000 ? "million" : "") + "</td></tr>";
-                // rowHtml += "<tr><td>" + arr_market_name[i] + "</td>";
+                + (tickers[i].acc_trade_price_24h > 1000000 ? "백만" : "") + "</td></tr>";
+
 
                 $("#table_ticker > tbody:last").append(rowHtml);
             }

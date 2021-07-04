2094b0e964a6fc00f2ed6113a4c4651f9ec495dd
YuA83 <goom0803@naver.com>
2021-07-04 23:17:21 +0900

    mytrade

--- routes/mytrade.js
+++ routes/mytrade.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const ejs = require('ejs');
 const router = express.Router();
-// 세션 포함하기
+const session = require('express-session');
+const client = require('./mysql');
+const moment = require('moment');
 
-router.get('/', function(req, res, next) {
-  if(req.session.logined == true){
-    res.render('mytrade',{
-      logined : true,
-      title : ejs.render('title')
-    });
-  }else{
-    res.render('mytrade',{
-      logined : false,
-      title : ejs.render('title')
-    });
-  }  
+router.use(session({
+    secret: 'blackzat',
+    resave: false,
+    saveUninitialized: true, 
+}));
+
+
+router.get('/', function(req, res) {
+    if(req.session.logined != true) {
+        res.send("<script language=\"javascript\">alert('[ ERROR ] : Please Login'); location.replace('/login');</script>");
+    } else {
+        client.query("select * from userdb where id=?", [req.session.userId], (error, result1) => {
+            client.query("select * from trade where id=? and coin_name is not null", [req.session.userId], (error, result2) => {
+                client.query("select * from wallet where id=?", [req.session.userId], (error, result3) => {
+                    res.render('mytrade',{
+                        title : ejs.render('title'),
+                        logined : true,
+                        moment: moment,
+                        balance: result1[0].balance,
+                        trade: result2,
+                        wallet: result3,
+                    });
+                    console.log(result2.money);
+                });
+            });
+        });
+    }  
 });
+
 
 module.exports = router;

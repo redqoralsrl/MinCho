e9aa5c24ce397eb10cc982480bd6ed4dc8069c00
redqoralsrl <redqoralsrl@gmail.com>
2021-07-05 12:07:12 +0900

    mytrade fin

--- routes/mytrade.js
+++ routes/mytrade.js
@@ -11,7 +11,6 @@ router.use(session({
     saveUninitialized: true, 
 }));
 
-
 router.get('/', function(req, res) {
     if(req.session.logined != true) {
         res.send("<script language=\"javascript\">alert('[ ERROR ] : Please Login'); location.replace('/login');</script>");
@@ -19,6 +18,12 @@ router.get('/', function(req, res) {
         client.query("select * from userdb where id=?", [req.session.userId], (error, result1) => {
             client.query("select * from trade where id=? and coin_name is not null", [req.session.userId], (error, result2) => {
                 client.query("select * from wallet where id=?", [req.session.userId], (error, result3) => {
+                    let totalMoney = 0;
+
+                    for(let i = 0; i < result3.length; i++) {
+                        let temp =  result3[i].wallet_coin_count * result3[i].avg_buy_price;
+                        totalMoney += temp;
+                    }
                     res.render('mytrade',{
                         title : ejs.render('title'),
                         logined : true,
@@ -26,13 +31,23 @@ router.get('/', function(req, res) {
                         balance: result1[0].balance,
                         trade: result2,
                         wallet: result3,
+                        totalMoney: totalMoney,
                     });
-                    console.log(result2.money);
                 });
             });
         });
     }  
 });
+
+router.post('/recv', function(req, res) {
+    client.query("select coin_name from wallet where id=?", [req.session.userId], (error, result) => {
+        res.send(result);
+    });
+});
+
+// router.post('/show', function(req, res) {
+//     console.log("눈누난나나나나", data);
+// });
 
 
 module.exports = router;
